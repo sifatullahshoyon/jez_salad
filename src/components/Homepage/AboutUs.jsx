74863a203fbb6e9f7e8d8fb027e557609631de5a
freelancersifat627e <freelancersifat627@gmail.comm>
2024-09-10 23:21:32 +0600

    slider work not finished

--- src/components/Homepage/AboutUs.jsx
+++ src/components/Homepage/AboutUs.jsx
@@ -1,31 +1,59 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import Container from "../Shared/Container";
 import Slider from "../Slider";
 
 const AboutUs = () => {
+  const swiperRef = useRef(null); // Ref to store Swiper instance
+
+  // Function to go to the next slide
+  const handleNextClick = () => {
+    if (swiperRef.current) swiperRef.current.slideNext();
+  };
+
+  // Function to go to the previous slide
+  const handlePrevClick = () => {
+    if (swiperRef.current) swiperRef.current.slidePrev();
+  };
+
   return (
     <section className="py-[100px]">
-      <div className="bg-gradient-to-r from-[#FFF6EA] to-[#FFE8C7]">
+      <div className="bg-gradient-to-r from-[#FFF6EA] to-[#FFE8C7] h-full">
         <Container>
-          <div className="flex flex-row  items-center h-[682px]">
-            <div>
-              <h1 className="text-5xl text-[#333333] font-semibold">
+          <div className="flex flex-col lg:flex-row items-center h-full lg:h-[682px] pl-5 lg:pl-5">
+            <div className="lg:w-1/2 w-full">
+              <h1 className="text-3xl lg:text-5xl text-[#333333] font-semibold">
                 What Are People <br />
                 Saying <span>About Us</span>
               </h1>
-              <p className="my-8 text-balance text-base text-[#999999] font-normal">
+              <p className="my-8 text-balance text-sm lg:text-base text-[#999999] font-normal">
                 We are very happy if you are satisfied with our <br /> service
                 and products, let&apos;s read pure reviews from <br /> customers
                 who bought our products.
               </p>
-              <p className="mt-6 text-[#333333] text-5xl font-bold">
-              02<small className="text-base text-[#CCCCCC]">/05</small>
-            </p>
+              <p className="mt-6 text-[#333333] text-3xl lg:text-5xl font-bold">
+                02<small className="text-base text-[#CCCCCC]">/05</small>
+              </p>
+              {/* Left and Right Buttons */}
+              <div className="mt-4">
+                <button
+                  onClick={handlePrevClick}
+                  className="mr-4 bg-gray-200 px-4 py-2 rounded-lg"
+                >
+                  Left
+                </button>
+                <button
+                  onClick={handleNextClick}
+                  className="bg-gray-200 px-4 py-2 rounded-lg"
+                >
+                  Right
+                </button>
+              </div>
             </div>
-            {/* Arrow Icon */}
-            <div>
-                {/* TODO: Slider work not finished */}
-              {/* <Slider></Slider> */}
+
+            {/* Carousel */}
+            <div className="lg:w-1/2 w-full mt-6 lg:mt-0">
+              <Slider swiperRef={swiperRef} />
             </div>
           </div>
         </Container>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,27 +1,54 @@
-'use client'
-import React from 'react';
-import { Pagination } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import Image from "next/image";
+import img from "/public/assets/images/icon/iconOne.png";
 
-const Slider = () => {
-    return (
-        <div>
-          <Swiper
-        slidesPerView={3}
+const Slider = ({ swiperRef }) => {
+  return (
+    <div className="relative w-full">
+      <Swiper
+        slidesPerView={1}
         spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
+        pagination={{ clickable: true }}
+        loop={true}
         modules={[Pagination]}
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper; // Assign Swiper instance to ref
+        }}
         className="mySwiper"
       >
         <SwiperSlide>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus corporis nam blanditiis, inventore eligendi ipsa illo consequuntur, harum quo perspiciatis error ut quis perferendis repudiandae voluptas quia vel facere nesciunt libero qui, expedita explicabo. Nam beatae unde neque eum quisquam quibusdam ipsam molestiae ab velit nulla ducimus ullam, porro totam iure aut, quam voluptatem minus expedita obcaecati praesentium eos iusto soluta? Doloribus, nulla ullam! Voluptates pariatur vel cum provident quis aspernatur exercitationem velit debitis eum tempore quod quidem sint veniam consequuntur deserunt enim nisi doloribus qui officia, labore fugiat laborum architecto odio fuga. Aperiam ratione neque reprehenderit, minima soluta numquam.
+          <div className="max-w-[350px] space-y-4 rounded-lg bg-white p-6 shadow-lg md:w-[350px]">
+            <div>
+              <Image src={img} alt="img" />
+            </div>
+            <div className="grid gap-2">
+              <h1 className="text-lg font-semibold ">Product Name</h1>
+              <p className="text-sm text-gray-500 dark:text-white/60">
+                This is a brief description of the product. It highlights the
+                key features and benefits.
+              </p>
+              <div className="text-lg font-semibold">$99.99</div>
+            </div>
+            <div className="flex gap-4">
+              <button className="rounded-lg bg-slate-800 px-6 py-2 text-[12px] font-semibold text-white duration-300 hover:bg-slate-950 sm:text-sm md:text-base ">
+                Add to Cart
+              </button>
+              <button className="rounded-md border border-black px-4 dark:border-white dark:hover:text-slate-800 dark:hover:bg-white  py-2  duration-300 hover:bg-gray-200">
+                View Details
+              </button>
+            </div>
+          </div>
         </SwiperSlide>
-        
+        {/* <SwiperSlide><h1>Slide 2</h1></SwiperSlide> */}
+        {/* <SwiperSlide><h1>Slide 3</h1></SwiperSlide> */}
       </Swiper>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Slider;
